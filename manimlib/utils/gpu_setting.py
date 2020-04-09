47956f0b7cafec66938bb3357a9693dc6e1b1533
Rangers <447192998@qq.com>
2020-04-09 19:50:10 +0800

    delete docs

--- manimlib/utils/gpu_setting.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def set_gpus(gpu_index):
-    if type(gpu_index) == list:
-        gpu_index = ','.join(str(_) for _ in gpu_index)
-    if type(gpu_index) ==int:
-        gpu_index = str(gpu_index)
-    os.environ["CUDA_VISIBLE_DEVICES"] = gpu_index
--- source/3_reference/mobject/mobject.rst
+++
@@ -1,9 +0,0 @@
-Mobject
-==========
-
-你好 
-
-.. automodule:: manimlib.mobject.mobject
-    :members: Mobject,Group
-
-.. autoclass:: manimlib.animation.animation.Animation
